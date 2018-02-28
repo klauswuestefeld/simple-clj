1bff105413c5d4f69da1beb8fb4059b08db145b6
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2018-02-28 16:16:44 -0300

    email-address project for email validation.

---
+++ email-address/project.clj
@@ -0,0 +1,11 @@
+(defproject simple/email-address "1.0.1"
+
+  :description "Email address validation."
+  :url "https://github.com/klauswuestefeld/simple-clj/tree/master/email-address"
+
+  :license {:name "BSD 3-Clause"
+            :url "https://github.com/klauswuestefeld/simple-clj/blob/master/LICENSE"}
+
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+
+  :repositories [["clojars" {:sign-releases false}]])
---
+++ email-address/src/simple/email_address1.clj
@@ -0,0 +1,7 @@
+(ns simple.email-address1)
+
+(def email-regex #"(?i)[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+
+(defn email?
+  "Returns address if it is a valid email, otherwise nil."
+  [address] (and address (re-matches email-regex (str address))))
