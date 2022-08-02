095b719822edcacdf50698fcf62ba0dc18a2627b
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2022-08-02 18:01:56 -0300

    test.twodee project created

--- test/script/project.clj
+++ test/script/project.clj
@@ -1,7 +1,7 @@
 (defproject house.jux/test.script "2022.01.18"
 
   :description "Support for event driven test scripts."
-  :url "https://github.com/klauswuestefeld/simple-clj/tree/master/test-script"
+  :url "https://github.com/klauswuestefeld/simple-clj/tree/master/test/script"
 
   :license {:name "BSD 3-Clause"
             :url "https://github.com/klauswuestefeld/simple-clj/blob/master/LICENSE"}
---
+++ test/twodee/project.clj
@@ -0,0 +1,12 @@
+(defproject house.jux/test.twodee "2022.08.02-SNAPSHOT"
+
+  :description "Support for highly expressive, two-dimensional tests represented as spreadsheets."
+  :url "https://github.com/klauswuestefeld/simple-clj/tree/master/test-script"
+
+  :license {:name "BSD 3-Clause"
+            :url "https://github.com/klauswuestefeld/simple-clj/blob/master/LICENSE"}
+
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [simple/check "2.0.0"]]
+
+  :repositories [["clojars" {:sign-releases false}]])
