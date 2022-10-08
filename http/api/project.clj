c898f5a16cb34cbcd2bb0ca1ff52f039e558dcf5
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2022-10-08 15:16:20 -0300

    Release http libs

--- http/api/project.clj
+++ http/api/project.clj
@@ -1,10 +1,14 @@
-(defproject house.jux/http.api "2022.08.03-SNAPSHOT"
+(defproject house.jux/http.api "2022.10.03"
 
   :description "Wrapper that handles API calls and redirects it to corresponding functions"
   :url "https://github.com/klauswuestefeld/simple-clj/tree/master/http/api"
 
   :license {:name "BSD 3-Clause"
             :url "https://github.com/klauswuestefeld/simple-clj/blob/master/LICENSE"}
+
+  :pom-addition [:properties
+                 ["maven.compiler.source" "11"]
+                 ["maven.compiler.target" "11"]]
 
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [cheshire "5.8.0"]]
--- http/pprint/project.clj
+++ http/pprint/project.clj
@@ -1,4 +1,4 @@
-(defproject house.jux/http.pprint "2022.08.03-SNAPSHOT"
+(defproject house.jux/http.pprint "2022.10.03"
 
   :description "Wrapper that prints summaries of incoming requests"
   :url "https://github.com/klauswuestefeld/simple-clj/tree/master/http/pprint"
@@ -6,6 +6,10 @@
   :license {:name "BSD 3-Clause"
             :url "https://github.com/klauswuestefeld/simple-clj/blob/master/LICENSE"}
 
+  :pom-addition [:properties
+                 ["maven.compiler.source" "11"]
+                 ["maven.compiler.target" "11"]]
+                 
   :dependencies [[org.clojure/clojure "1.11.1"]]
 
   :repositories [["clojars" {:sign-releases false}]])
