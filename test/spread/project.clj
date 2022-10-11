86ed51c677f51849a463d14d5a4934178fdfea76
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2022-10-11 00:11:06 -0300

    spread release 2022.10.13

--- test/spread/project.clj
+++ test/spread/project.clj
@@ -1,4 +1,4 @@
-(defproject house.jux/test.spread "2022.10.12"
+(defproject house.jux/test.spread "2022.10.13"
 
   :description "Support for highly expressive, two-dimensional tests represented as spreadsheets."
   :url "https://github.com/klauswuestefeld/simple-clj/tree/master/test/spread"
