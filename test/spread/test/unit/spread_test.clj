61c58063b20cf44a4badf3b370a4558e55ecfe0b
Kauwai Lucchesi <kauwainobre@outlook.com>
2022-11-09 11:24:35 -0300

    Updates test to reflect spreadsheet changes

--- test/spread/test/unit/spread_test.clj
+++ test/spread/test/unit/spread_test.clj
@@ -7,40 +7,66 @@
 ;; TODO: fix the following test
 (deftest building-test-map
   (is (= {:title "A user can create a new profile",
-          :queries [["tester" "created-profile" ":name"]
-                    ["tester" "created-profile" ":phone"]
-                    ["tester" "created-profile" ":age"]
-                    ["tester" "created-profile" ":created-by"]
-                    ["tester" "created-profile" ":last-updated-by"]
-                    ["tester" "search-by-name \"Bob\"" ":last-updated-by"]],
+          :queries
+          [["tester" "created-profile" ":name" "" ""]
+           ["tester" "created-profile" ":phone" "" ""]
+           ["tester" "created-profile" ":age" "" ""]
+           ["tester" "created-profile" ":created-by" "" ""]
+           ["tester" "created-profile" ":last-updated-by" "" ""]
+           ["tester" "search-by-name \"Bob\"" ":last-updated-by" "" ""]
+           ["tester" "created-profile" "map first _" "sort" "take 2 _"]
+           ["tester" "created-profile" "sort (map first _)" "take 2 _" "vec [_]"]],
           :steps
           [{:command
             {:user "nil",
              :function "(fn [state & _ignored] state)",
              :params "",
              :result "*",
-             :result-coords {:line 5, :column "D"}},
-            :query-results ["nil" "nil" "nil" "nil" "nil" "nil"]}
+             :result-coords {:line 7, :column "D"}},
+            :query-results ["nil" "nil" "nil" "nil" "nil" "nil" "[]" "[[]]"]}
            {:command
             {:user "tester",
              :function "create-new-profile",
-             :params "{:name \"Bob\" :phone 444 :age 40}",
+             :params "{:name \"Bob\"\n :phone 444\n :age 40}",
              :result "*",
-             :result-coords {:line 6, :column "D"}},
-            :query-results ["\"Bob\"" "444" "40" "tester" "nil" "nil"]}
+             :result-coords {:line 8, :column "D"}},
+            :query-results
+            ["\"Bob\""
+             "444"
+             "40"
+             "tester"
+             "nil"
+             "nil"
+             "[:age :created-by]"
+             "[[:age :created-by]]"]}
            {:command
             {:user "tester",
              :function "update-profile",
              :params "{:phone 555}",
              :result "*",
-             :result-coords {:line 7, :column "D"}},
-            :query-results ["\"Bob\"" "555" "40" "tester" "tester" "tester"]}
+             :result-coords {:line 9, :column "D"}},
+            :query-results
+            ["\"Bob\""
+             "555"
+             "40"
+             "tester"
+             "tester"
+             "tester"
+             "[:age :created-by]"
+             "[[:age :created-by]]"]}
            {:command
             {:user "tester",
              :function "update-profile",
              :params "{:name \"BOOM\"}",
              :result "Invalid name",
-             :result-coords {:line 8, :column "D"}},
+             :result-coords {:line 10, :column "D"}},
             :query-results
-            ["\"Bob\"" "555" "40" "tester" "tester" "tester"]}]}
+            ["\"Bob\""
+             "555"
+             "40"
+             "tester"
+             "tester"
+             "tester"
+             "[:age :created-by]"
+             "[[:age :created-by]]"]}]}
          (subject/csv->test-map parsed-csv))))
