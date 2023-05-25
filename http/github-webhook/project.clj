22488e59e3dffcd6a9e395a9ea036180906660d4
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2023-05-25 15:44:13 -0300

    github-webhook 0.0.1

---
+++ http/github-webhook/project.clj
@@ -0,0 +1,16 @@
+(defproject house.jux/http.github-webhook "0.0.1"
+
+  :description "Github webhook for redeploy."
+  :url "https://github.com/klauswuestefeld/simple-clj/tree/master/http/github-webhook"
+
+  :license {:name "BSD 3-Clause"
+            :url "https://github.com/klauswuestefeld/simple-clj/blob/master/LICENSE"}
+
+  :pom-addition [:properties
+                 ["maven.compiler.source" "17"]
+                 ["maven.compiler.target" "17"]]
+
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [cheshire "5.8.0"]]
+
+  :repositories [["clojars" {:sign-releases false}]])
