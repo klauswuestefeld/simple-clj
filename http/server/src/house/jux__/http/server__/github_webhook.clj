26a0196de1ae383021cb4e77f42e7a5d2f42c7bc
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2023-06-14 16:32:47 -0300

    http.server-2023.06.14.16.2

--- http/server/src/house/jux__/http/server__/github_webhook.clj
+++ http/server/src/house/jux__/http/server__/github_webhook.clj
@@ -6,22 +6,22 @@
 (def secret (System/getenv "GITHUB_WEBHOOK_SECRET"))
 (when-not secret (prn "GITHUB_WEBHOOK_SECRET env variable must be set"))
 
-(defn- valid-webhook-request? [{:strs [uri params]}]
-  (and
-   (some->> secret (.endsWith uri))
-   (let [parsed-payload (json/parse-string (get params "payload") keyword)]
-     (= (get parsed-payload "ref")
-        "refs/heads/main"))))
+(defn- main-branch? [{:strs [payload]}]
+  (-> payload
+      json/parse-string
+      (get "ref")
+      (= "refs/heads/main")))
 
-(defn- redeploy! [service-name on-redeploy]
-  (future
-    (when (-> (shell/sh "git" "pull") :exit zero?)
-      (on-redeploy)
-      (shell/sh "systemctl" "restart" service-name)))
+(defn- redeploy-if-necessary! [service-name on-redeploy params]
+  (when (main-branch? params)
+    (future
+      (when (-> (shell/sh "git" "pull") :exit zero?)
+        (on-redeploy)
+        (shell/sh "systemctl" "restart" service-name))))
   {:status 202})
 
 (defn wrap-redeploy! [delegate service-name on-redeploy]
-  (fn [request]
-    (if (valid-webhook-request? request)
-      (redeploy! service-name on-redeploy)
+  (fn [{:as request :keys [uri params]}]
+    (if (some->> secret (.endsWith uri))
+      (redeploy-if-necessary! service-name on-redeploy params)
       (delegate request))))
