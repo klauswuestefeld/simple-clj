e9694751a2ae604d619301b25b5096b900506334
Kauwai Lucchesi <kauwainobre@outlook.com>
2024-04-26 11:55:46 -0300

    Rename vars + update description and clojure version

--- biz/command-result/src/house/jux__/biz/command_result__.clj
+++ biz/command-result/src/house/jux__/biz/command_result__.clj
@@ -2,16 +2,19 @@
   (:require [simple.check2 :refer [check]]))
 
 #_{:clj-kondo/ignore [:uninitialized-var]}
-(def ^:dynamic *result-atom*)
+(def ^:dynamic *result*)
 
-(defn new-result-atom []
+#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
+(defn reset-result []
   (atom ::no-result))
 
-(defn peek-result []
-  (let [result @*result-atom*]
+#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
+(defn get-result []
+  (let [result @*result*]
     (if (= result ::no-result) nil result)))
 
-(defn set-result-atom [v]
-  (when (bound? #'*result-atom*)
-    (check (= @*result-atom* ::no-result) (str "Transaction result was already set: " (if-some [result @*result-atom*] result "nil")))
-    (reset! *result-atom* v)))
+#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
+(defn set-result [v]
+  (when (bound? #'*result*)
+    (check (= @*result* ::no-result) (str "Transaction result was already set: " (if-some [result @*result*] result "nil")))
+    (reset! *result* v)))
