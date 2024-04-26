2ae6d8fd88a4192aa2c550fc388b547ea1749a84
Kauwai Lucchesi <kauwainobre@outlook.com>
2024-04-26 11:55:46 -0300

    Rename vars + update description and clojure version

--- biz/command-result/project.clj
+++ biz/command-result/project.clj
@@ -1,6 +1,6 @@
-(defproject house.jux/biz.command-result "2024.04.25"
+(defproject house.jux/biz.command-result "2024.04.26-SNAPSHOT"
 
-  :description "Support for *result-atom* (a dynamic var)."
+  :description "Support for commands to return a result without polluting the state (uses a dynamic var)."
   :url "https://github.com/klauswuestefeld/simple-clj/tree/master/biz/command-result"
 
   :license {:name "BSD 3-Clause"
@@ -10,5 +10,5 @@
                  ["maven.compiler.source" "11"]
                  ["maven.compiler.target" "11"]]
 
-  :dependencies [[org.clojure/clojure "1.11.1"]
+  :dependencies [[org.clojure/clojure "1.11.3"]
                  [simple/check "2.0.0"]])
