86a22c007dca7a546d0fc6b70ba835bed7b335f6
Kauwai Lucchesi <kauwainobre@outlook.com>
2024-05-27 17:31:07 -0300

    Create treated-check

---
+++ treated-check/project.clj
@@ -0,0 +1,9 @@
+(defproject house.jux/treated-check "2023.05.27-SNAPSHOT"
+
+  :description "An assertion that cannot be turned off, for predicted situations."
+  :url "https://github.com/klauswuestefeld/simple-clj/tree/master/treated-check"
+
+  :license {:name "BSD 3-Clause"
+            :url "https://github.com/klauswuestefeld/simple-clj/blob/master/LICENSE"}
+
+  :dependencies [[org.clojure/clojure "1.11.1"]])
