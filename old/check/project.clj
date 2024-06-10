9398a33d9e320a51fefb0fc040d7f1abe2bd0120
Kauwai Lucchesi <kauwainobre@outlook.com>
2024-06-10 17:42:21 -0300

    Update project license

--- old/check/project.clj
+++ old/check/project.clj
@@ -1,10 +1,13 @@
-(defproject simple/check "2024.06.09"
+(defproject simple/check "2024.06.10"
 
   :description "An assertion that cannot be turned off."
   :url "https://github.com/klauswuestefeld/simple-clj/tree/master/check"
 
-  :plugins [[lein-parent "0.3.9"]]
-  :parent-project {:coords  [house.jux/parent-project "2024.06.09"]
-                   :inherit [:deploy-repositories :dependencies :license]}
+  :license {:name "BSD 3-Clause"
+            :url "https://github.com/klauswuestefeld/simple-clj/blob/master/LICENSE"}
 
-  :dependencies [[house.jux/exceptions "2024.06.09"]])
+  :plugins [[lein-parent "0.3.9"]]
+  :parent-project {:coords  [house.jux/parent-project "2024.06.10"]
+                   :inherit [:deploy-repositories :dependencies]}
+
+  :dependencies [[house.jux/exceptions "2024.06.10"]])
