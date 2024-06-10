0b9b122f34687dd6cc5ba81fe75f371b4de1f651
Kauwai Lucchesi <kauwainobre@outlook.com>
2024-06-10 14:49:01 -0300

    Update http.exceptions project.clj to inherit from parent

--- http/exceptions/project.clj
+++ http/exceptions/project.clj
@@ -1,15 +1,11 @@
-(defproject house.jux/http.exceptions "2024.05.28-SNAPSHOT"
+(defproject house.jux/http.exceptions "2024.06.09"
 
   :description "Wrapper that handles API thrown exceptions with a 400 status code"
   :url "https://github.com/klauswuestefeld/simple-clj/tree/master/http/exceptions"
 
-  :license {:name "BSD 3-Clause"
-            :url "https://github.com/klauswuestefeld/simple-clj/blob/master/LICENSE"}
+  :plugins [[lein-parent "0.3.9"]]
+  :parent-project {:coords  [house.jux/parent-project "2024.06.09"]
+                   :inherit [:deploy-repositories :dependencies :license]}
 
-  :pom-addition [:properties
-                 ["maven.compiler.source" "11"]
-                 ["maven.compiler.target" "11"]]
-
-  :dependencies [[org.clojure/clojure "1.11.1"]
-                 [cheshire "5.8.0"]
-                 [house.jux/exceptions "2024.05.28-SNAPSHOT"]])
+  :dependencies [[cheshire "5.8.0"]
+                 [house.jux/exceptions "2024.06.09"]])
