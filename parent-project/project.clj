fc1a6412bb0d9fd08b97f67371a810f1b3f2c6c9
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2024-06-10 12:27:56 -0300

    Update parent-project

--- parent-project/project.clj
+++ parent-project/project.clj
@@ -1,5 +1,8 @@
-(defproject parent-project "2024.06.07"
+(defproject house.jux/parent-project "2024.06.07"
   :description "Parent project for shared configuration"
+
+  :dependencies [[org.clojure/clojure "1.11.3"]]
+
   :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
                                     :username :env/CLOJARS_USERNAME
                                     :password :env/CLOJARS_PASSWORD
