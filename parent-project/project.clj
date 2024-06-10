76435dc6b38b768c462fa9387c346777e57bbcc4
Kauwai Lucchesi <kauwainobre@outlook.com>
2024-06-10 17:28:54 -0300

    Update license

--- parent-project/project.clj
+++ parent-project/project.clj
@@ -1,10 +1,10 @@
-(defproject house.jux/parent-project "2024.06.09"
+(defproject house.jux/parent-project "2024.06.10"
   :description "Parent project for shared configuration"
 
   :dependencies [[org.clojure/clojure "1.11.3"]]
 
-  :license {:name "BSD 3-Clause"
-            :url "https://github.com/klauswuestefeld/simple-clj/blob/master/LICENSE"}
+  :license {:name "Proprietary"
+            :url "https://en.wikipedia.org/wiki/Proprietary_software"}
 
   :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
                                     :username :env/CLOJARS_USERNAME
