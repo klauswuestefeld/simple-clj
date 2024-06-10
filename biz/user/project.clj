cd2f04c55fd8361ee754eba91afb5f90a6b7731c
Kauwai Lucchesi <kauwainobre@outlook.com>
2024-06-10 17:29:55 -0300

    Update biz.user to use parent-project settings

--- biz/user/project.clj
+++ biz/user/project.clj
@@ -1,4 +1,4 @@
-(defproject house.jux/biz.user "2022.10.03"
+(defproject house.jux/biz.user "2024.06.10"
 
   :description "Support for an implicit *user* argument (a dynamic var)."
   :url "https://github.com/klauswuestefeld/simple-clj/tree/master/biz/user"
@@ -6,8 +6,6 @@
   :license {:name "BSD 3-Clause"
             :url "https://github.com/klauswuestefeld/simple-clj/blob/master/LICENSE"}
 
-  :pom-addition [:properties
-                 ["maven.compiler.source" "11"]
-                 ["maven.compiler.target" "11"]]
-
-  :dependencies [[org.clojure/clojure "1.11.1"]])
+  :plugins [[lein-parent "0.3.9"]]
+  :parent-project {:coords  [house.jux/parent-project "2024.06.10"]
+                   :inherit [:deploy-repositories :dependencies]})
