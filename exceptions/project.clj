3c8f4b42ae58f80794d34239aa25d727a8a048c9
Kauwai Lucchesi <kauwainobre@outlook.com>
2024-06-10 14:40:20 -0300

    Update project.clj using parent common configs

--- exceptions/project.clj
+++ exceptions/project.clj
@@ -1,18 +1,7 @@
-(defproject house.jux/exceptions "2024.06.08"
-
-  
+(defproject house.jux/exceptions "2024.06.09"
   :description "Exception utilities"
   :url "https://github.com/klauswuestefeld/simple-clj/tree/master/exceptions"
 
-  :plugins [[lein-parent "0.3.9"]] 
-  :parent-project {:path "../_parent-project/project.clj"
-                   :inherit [:deploy-repositories]}
-
-  :license {:name "BSD 3-Clause"
-            :url "https://github.com/klauswuestefeld/simple-clj/blob/master/LICENSE"}
-  
-  :dependencies [[org.clojure/clojure "1.11.1"]]
-
-  :pom-addition [:properties
-                 ["maven.compiler.source" "17"]
-                 ["maven.compiler.target" "17"]])
+  :plugins [[lein-parent "0.3.9"]]
+  :parent-project {:coords  [house.jux/parent-project "2024.06.09"]
+                   :inherit [:deploy-repositories :dependencies :license]})
