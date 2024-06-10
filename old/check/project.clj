d125502013b2e8df6cbc748be536c3dd95abde19
Kauwai Lucchesi <kauwainobre@outlook.com>
2024-06-10 14:55:43 -0300

    Update check to inherit from parent-project

--- old/check/project.clj
+++ old/check/project.clj
@@ -1,10 +1,10 @@
-(defproject simple/check "2024.05.28-SNAPSHOT"
+(defproject simple/check "2024.06.09"
 
   :description "An assertion that cannot be turned off."
   :url "https://github.com/klauswuestefeld/simple-clj/tree/master/check"
 
-  :license {:name "BSD 3-Clause"
-            :url "https://github.com/klauswuestefeld/simple-clj/blob/master/LICENSE"}
+  :plugins [[lein-parent "0.3.9"]]
+  :parent-project {:coords  [house.jux/parent-project "2024.06.09"]
+                   :inherit [:deploy-repositories :dependencies :license]}
 
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [house.jux/exceptions "2024.05.28-SNAPSHOT"]])
+  :dependencies [[house.jux/exceptions "2024.06.09"]])
