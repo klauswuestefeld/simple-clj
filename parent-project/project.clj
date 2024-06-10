4d1f01d95db967f6990fed4a3d3b56ba0d9fcadd
Kauwai Lucchesi <kauwainobre@outlook.com>
2024-06-10 14:21:16 -0300

    Fix license url

--- parent-project/project.clj
+++ parent-project/project.clj
@@ -1,10 +1,10 @@
-(defproject house.jux/parent-project "2024.06.08"
+(defproject house.jux/parent-project "2024.06.09"
   :description "Parent project for shared configuration"
 
   :dependencies [[org.clojure/clojure "1.11.3"]]
 
-  :license {:name "BSD 3-Clause"}
-            :url "https://github.com/klauswuestefeld/simple-clj/blob/master/LICENSE"
+  :license {:name "BSD 3-Clause"
+            :url "https://github.com/klauswuestefeld/simple-clj/blob/master/LICENSE"}
 
   :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
                                     :username :env/CLOJARS_USERNAME
