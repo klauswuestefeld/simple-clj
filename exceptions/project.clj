eda12fb484b66e84a6e6f16d3cbbaedc264fff7d
Kauwai Lucchesi <kauwainobre@outlook.com>
2024-06-10 17:32:24 -0300

    Update project license

--- exceptions/project.clj
+++ exceptions/project.clj
@@ -1,7 +1,10 @@
-(defproject house.jux/exceptions "2024.06.09"
+(defproject house.jux/exceptions "2024.06.10"
   :description "Exception utilities"
   :url "https://github.com/klauswuestefeld/simple-clj/tree/master/exceptions"
 
+  :license {:name "BSD 3-Clause"
+            :url "https://github.com/klauswuestefeld/simple-clj/blob/master/LICENSE"}
+
   :plugins [[lein-parent "0.3.9"]]
-  :parent-project {:coords  [house.jux/parent-project "2024.06.09"]
-                   :inherit [:deploy-repositories :dependencies :license]})
+  :parent-project {:coords  [house.jux/parent-project "2024.06.10"]
+                   :inherit [:deploy-repositories :dependencies]})
