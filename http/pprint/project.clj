f9416d9abdba3cd03d71ccc172139a2a749ea512
Kauwai Lucchesi <kauwainobre@outlook.com>
2024-06-10 15:12:11 -0300

    Update pprint project.clj to use its parents config

--- http/pprint/project.clj
+++ http/pprint/project.clj
@@ -1,13 +1,8 @@
-(defproject house.jux/http.pprint "2024.05.28-SNAPSHOT"
+(defproject house.jux/http.pprint "2024.06.09"
 
   :description "Wrapper that prints summaries of incoming requests"
   :url "https://github.com/klauswuestefeld/simple-clj/tree/master/http/pprint"
 
-  :license {:name "BSD 3-Clause"
-            :url "https://github.com/klauswuestefeld/simple-clj/blob/master/LICENSE"}
-
-  :pom-addition [:properties
-                 ["maven.compiler.source" "11"]
-                 ["maven.compiler.target" "11"]]
-                 
-  :dependencies [[org.clojure/clojure "1.11.1"]])
+  :plugins [[lein-parent "0.3.9"]]
+  :parent-project {:coords  [house.jux/parent-project "2024.06.09"]
+                   :inherit [:deploy-repositories :dependencies :license]})
