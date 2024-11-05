585f7804622765936c37bed6c2a09df293067e30
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2024-11-05 12:44:07 -0300

    profiler: classes -> packages

--- tools/sampling-profiler/src/house/jux__/tools/sampling_profiler__.clj
+++ tools/sampling-profiler/src/house/jux__/tools/sampling_profiler__.clj
@@ -1,23 +1,34 @@
 (ns house.jux--.tools.sampling-profiler--
   #_(:require [clojure.pprint :refer [pprint]]))
 
+(def empty-summary {:sample-count 0})
+
 (def summary (atom nil))
 (def running (atom false))
 (defn stop! []
   (reset! running false))
 
-(defn- tally-classes [summary stack]
+
+(defn- package-name [stack-frame]
+  (let [fully-qualified-class (.getClassName stack-frame)
+        last-dot (.lastIndexOf fully-qualified-class ".")]
+    (if (pos? last-dot)
+      (.substring fully-qualified-class 0 last-dot)
+      nil)))
+
+(defn- tally-package [summary package]
+  (update-in summary [:package->count package] (fnil inc 0)))
+
+(defn- tally-packages [summary stack]
   (->> (seq stack)
        (remove nil?)
-       (map #(.getClassName %))
-       (reduce (fn [summary class-name]
-                 (update-in summary [:class->count class-name] (fnil inc 0)))
-               summary)))
+       (map package-name)
+       (reduce tally-package summary)))
 
 (defn- summarize [summary thread->stack]
   (as-> summary _
     (update _ :sample-count inc)
-    (reduce tally-classes _ (vals thread->stack))))
+    (reduce tally-packages _ (vals thread->stack))))
 
 (defn- summarize-samples! [{:keys [duration-sec interval-ms]}]
   (let [end-time (+ (System/currentTimeMillis) (* 1000 duration-sec))]
@@ -32,8 +43,8 @@
 (defn start! [options]
   (when @running
     (throw (IllegalStateException. "Profiler was already running.")))
-  (reset! summary {:sample-count 0})
   (reset! running true)
+  (reset! summary empty-summary)
   (future
     (try
       (summarize-samples! options)
@@ -42,9 +53,9 @@
 
 
 (comment
-  ; This will sample all thread stack frames every 20 milliseconds for a duration of 10 seconds. Useful for when the code being profiled hangs.
+  ; Example usage: This will sample all thread stack frames every 20 milliseconds for a duration of 3 seconds. Useful for when the code being profiled hangs.
   (start! {:interval-ms 20
-           :duration-sec 10})
+           :duration-sec 3})
 
   ; Run the code you want to profile...
 
