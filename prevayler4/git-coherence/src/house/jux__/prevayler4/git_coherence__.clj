6e1975f5b8d948d2c4eec6b69408292553cf9d2d
Rui Fernando Hayashi <rfhayashi@gmail.com>
2025-02-13 12:25:57 -0300

    improve doc

--- prevayler4/git-coherence/src/house/jux__/prevayler4/git_coherence__.clj
+++ prevayler4/git-coherence/src/house/jux__/prevayler4/git_coherence__.clj
@@ -100,8 +100,8 @@
                 currently only one level directory is supported 
                 (e.g. \"src\" is ok, but \"src/clj\" is not
      - refreshable-namespaces: a sequence of namespace symbols that need to be refreshed.
-                               All namespaces under each given symbol will be refreshed
-                               (e.g. ['my-system.biz] will refresh 'my-system.biz, 
+                               All namespaces whose names start with one of the given symbols
+                               will be refreshed (e.g. ['my-system.biz] will refresh 'my-system.biz,
                                'my-system.biz.somenamespace and so on)"
   [start-prevayler-fn config {:keys [coherent-mode? git-reset?] :as opts}]
   (when git-reset?
