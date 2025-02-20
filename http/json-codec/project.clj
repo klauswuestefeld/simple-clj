b51966385653b2b7b08fa4054e9653693aec2967
Kauwai Lucchesi <kauwainobre@outlook.com>
2025-02-20 17:56:14 -0300

    Add missing dependency to http.json-codec/project.clj

--- http/json-codec/project.clj
+++ http/json-codec/project.clj
@@ -1,4 +1,4 @@
-(defproject house.jux/http.json-codec "2024.12.16"
+(defproject house.jux/http.json-codec "2025.02.20"
 
   :description "Ring-style wrapper to transparently encode and decode JSON to/from Clojure values."
   :url "https://github.com/klauswuestefeld/simple-clj/tree/master/http/json-codec"
@@ -8,4 +8,5 @@
 
   :plugins [[lein-parent "0.3.9"]]
   :parent-project {:coords  [house.jux/parent-project "2024.06.10"]
-                   :inherit [:deploy-repositories :dependencies]})
+                   :inherit [:deploy-repositories :dependencies]}
+  :dependencies [[cheshire "5.8.0"]])
